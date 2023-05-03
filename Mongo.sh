388aa311c8a548f9312c47537139b41bfea52f7e
Gowthami <kattagowthami1998@gmail.com>
2023-05-03 11:49:46 +0530

    Added echo stmts , corrected cpy path for catalogue service and updated sed command

--- Mongo.sh
+++ Mongo.sh
@@ -1,6 +1,8 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
 
-#Edit the conf file from 127.0.0.1 to 0.0.0.0
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+systemctl enable mongod
+systemctl restart mongod
+
+#Edited the conf file from 127.0.0.1 to 0.0.0.0
--- redis.sh
+++ redis.sh
@@ -1,7 +1,15 @@
+echo -e "/e[36m>>>>Install redis repo<<<</e]0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+
+echo -e "/e[36m>>>>Install redis repo<<<</e]0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
+
+echo -e "/e[36m>>>>update listen address<<<</e]0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+
+echo -e "/e[36m>>>>start redis service<<<</e]0m"
 systemctl enable redis
-systemctl start redis
+systemctl restart redis
 
 # need to change port from 127.0.0.1 to 0.0.0.0
