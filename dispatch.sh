a81fccac4c97f8ad9765737ccfd3df70d141adec
Gowthami <kattagowthami1998@gmail.com>
2023-05-03 23:57:55 +0530

    crcted service path in cp cmd

--- dispatch.sh
+++ dispatch.sh
@@ -1,5 +1,6 @@
 yum install golang -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
 cd /app
@@ -8,7 +9,7 @@ cd /app
 go mod init dispatch
 go get
 go build
-cp dispatch.service /etc/systemd/system/dispatch.service
+cp /home/centos/roboshop-shell/dispatch.service /etc/systemd/system/dispatch.service
 systemctl daemon-reload
 systemctl enable dispatch
-systemctl start dispatch
+systemctl restart dispatch
