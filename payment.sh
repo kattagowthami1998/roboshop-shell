4013be9387ae1b5ceedddb27725bd0e09b3488e3
Gowthami <kattagowthami1998@gmail.com>
2023-05-03 22:36:32 +0530

    updated

--- payment.sh
+++ payment.sh
@@ -4,7 +4,6 @@ mkdir /app
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 cd /app
 unzip /tmp/payment.zip
-cd /app
 pip3.6 install -r requirements.txt
 cp home/centos/roboshop-shell/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
