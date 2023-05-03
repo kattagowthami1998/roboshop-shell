f718732ab22e386796a412706623c94b9fb4be2d
Gowthami <kattagowthami1998@gmail.com>
2023-05-03 22:31:44 +0530

    updated mysql.conf sed n crcted paths for service

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,20 @@
+echo -e "/e[36m>>>>Disable MYSQL<<</e]0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "/e[36m>>>>copying mysql.repo<<</e]0m"
+cp /home/centos/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo -e "/e[36m>>>>update listen address <<</e]0m"
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf
+
+echo -e "/e[36m>>>>install mysql.repo<<</e]0m"
 yum install mysql-community-server -y
+
+echo -e "/e[36m>>>>start mysql<<</e]0m"
 systemctl enable mysqld
-systemctl start mysqld
+systemctl restart mysqld
+
+echo -e "/e[36m>>>>set userid & pw<<</e]0m"
 mysql_secure_installation --set-root-pass RoboShop@1
 mysql -uroot -pRoboShop@1
--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,34 @@
+echo -e "/e[36m>>>>Install Maven<<<</e]0m"
 yum install maven -y
+
+echo -e "/e[36m>>>>Add user<<<</e]0m"
 useradd roboshop
+
+echo -e "/e[36m>>>>create app directory<<<</e]0m"
+rm -rf /app
 mkdir /app
+
+echo -e "/e[36m>>>>download app content<<<</e]0m"
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
 cd /app
+
+echo -e "/e[36m>>>>Extract app content<<<</e]0m"
 unzip /tmp/shipping.zip
-cd /app
+
+echo -e "/e[36m>>>>Download maven dependencies<<</e]0m"
 mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
+echo -e "/e[36m>>>>Install MYSQL<<</e]0m"
+yum install mysql -y
+
+echo -e "/e[36m>>>>Load schema<<</e]0m"
+mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
+
+echo -e "\e[36m>>>>copy catalogue service<<<<\e]0m"
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
+
+echo -e "/e[36m>>>>start shipping service<<</e]0m"
 systemctl daemon-reload
 systemctl enable shipping
 systemctl start shipping
-
-yum install mysql -y
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/shipping.sql
-systemctl restart shipping
