c338be247e8d613ceae846cf9c63e6d6998d4ed9
Gowthami <kattagowthami1998@gmail.com>
2023-05-07 23:56:36 +0530

    Added required func

--- shipping.sh
+++ shipping.sh
@@ -3,8 +3,8 @@ script_path=$(dirname "$script")
 source ${script_path}/common.sh
 mysql_root_password=$1
 
-if [ -z "$my_sql_root_password" ]; then
-  echo input MYSQL root password missing
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root Password Missing
   exit
 fi
 
