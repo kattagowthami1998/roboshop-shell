765013f6639e5ef2b1c7953cbf0aea86436cbfaf
Gowthami <kattagowthami1998@gmail.com>
2023-05-07 23:23:30 +0530

    Added required func

--- common.sh
+++ common.sh
@@ -5,50 +5,84 @@ print_head(){
 }
 
 func_schema_setup(){
-  if [ "$schema_setup" == "mongo" ]; then
-  print_head "Copy Mongo repo"
-  cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
+    if [ "$schema_setup" == "mongo" ]; then
+    print_head "Copy Mongo repo"
+    cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 
-  print_head "Install Mongo client"
- yum install mongodb-org-shell -y
+    print_head "Install Mongo client"
+   yum install mongodb-org-shell -y
 
-  print_head "Load schema"
-  mongo --host mongodb-dev.gowthamidevops.online </app/schema/${component}.js
+    print_head "Load schema"
+    mongo --host mongodb-dev.gowthamidevops.online </app/schema/${component}.js
 
-  fi
+    fi
+
+   if [ "$schema_setup" == "mysql" ]; then
+
+    print_head "Install MYSQL"
+    yum install mysql -y
+
+    print_head "Load schema"
+    mysql -h  mysql-dev.gowthamidevops.online -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+
+
+func_app_prereq() {
+      print_head "Add application user"
+      useradd ${app_user}
+
+      print_head "create application directory"
+      rm -rf /app
+      mkdir /app
+
+      print_head "Download app content"
+      curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+
+      print_head "unzip app content"
+      cd /app
+      unzip /tmp/${component}.zip
 }
 
-func_nodejs() {
- print_head "Configure Nodejs repo"
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-
- print_head "install Nodejs repo"
-  yum install nodejs -y
-
-  print_head "Add application user"
-  useradd ${app_user}
-
-  print_head "create application directory"
-  rm -rf /app
-  mkdir /app
-
-  print_head "Download app content"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
-
-  print_head "unzip app content"
-  cd /app
-  unzip /tmp/${component}.zip
-
-  print_head "Install Nodejs dependencies"
-  npm install
+func_systemd_setup() {
 
   print_head "copy catalogue service"
   cp $script_path/${component}.service /etc/systemd/system/${component}.service
 
-  print_head "start catalogue service"
-  systemctl daemon-reload
-  systemctl enable ${component}
-  systemctl restart ${component}
+   print_head "start catalogue service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
+    }
 
+
+func_nodejs() {
+
+   print_head "Configure Nodejs repo"
+   curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+    print_head "install Nodejs repo"
+    yum install nodejs -y
+
+    func_app_prereq
+
+    print_head "Install Nodejs dependencies"
+    npm install
+
+    func_systemd_setup
+    func_schema_setup
+}
+
+func_java() {
+
+  print_head "Install Maven"
+  yum install maven -y
+
+  func_app_prereq
+
+  print_head "Download maven dependencies"
+  mvn clean package
+  mv target/shipping-1.0.jar shipping.jar
+
+  func_systemd_setup
   func_schema_setup
+
 }
--- shipping.sh
+++ shipping.sh
@@ -1,38 +1,13 @@
 script=$(real_path "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
 
-echo -e "/e[36m>>>>Install Maven<<<</e]0m"
-yum install maven -y
+if [ -z "$my_sql_root_password" ]; then
+  echo input MYSQL root password missing
+  exit
+  fi
 
-echo -e "/e[36m>>>>Add user<<<</e]0m"
-useradd ${app_user}
-
-echo -e "/e[36m>>>>create app directory<<<</e]0m"
-rm -rf /app
-mkdir /app
-
-echo -e "/e[36m>>>>download app content<<<</e]0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-
-echo -e "/e[36m>>>>Extract app content<<<</e]0m"
-unzip /tmp/shipping.zip
-
-echo -e "/e[36m>>>>Download maven dependencies<<</e]0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-echo -e "/e[36m>>>>Install MYSQL<<</e]0m"
-yum install mysql -y
-
-echo -e "/e[36m>>>>Load schema<<</e]0m"
-mysql -h  mysql-dev.gowthamidevops.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-echo -e "\e[36m>>>>copy catalogue service<<<<\e]0m"
-cp ${script_path}/shipping.service /etc/systemd/system/shipping.service
-
-echo -e "/e[36m>>>>start shipping service<<</e]0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl start shipping
+component=shiping
+schema_setup=mysql
+func_java
