1118a64a529f115d9d2774d18f6e36edd274b6b6
Gowthami <kattagowthami1998@gmail.com>
2023-05-10 01:04:38 +0530

    Added -ne in func_app_prereq

--- common.sh
+++ common.sh
@@ -50,7 +50,7 @@ func_schema_setup() {
 func_app_prereq() {
       func_print_head "Add application user"
       id ${app_user} &>>$log_file
-      if [ $? ne 0 ]; then
+      if [ $? -ne 0 ]; then
       useradd ${app_user} &>>$log_file
       fi
       func_stat_check $?
