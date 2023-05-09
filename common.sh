1c1edfbe058a09a4681441dc56aa81e6f3ce7e47
Gowthami <kattagowthami1998@gmail.com>
2023-05-09 23:36:35 +0530

    Added required change to -eq in if condition

--- common.sh
+++ common.sh
@@ -9,9 +9,9 @@ func_print_head(){
   echo -e "\e[35m>>>>>> $1 <<<<<<\e[0m" &>>$log_file
 }
 
-func_stat_check(){
+func_stat_check() {
 
-      if[ $1 eq 0 ]; then
+      if[ $1 -eq 0 ]; then
       echo -e "\e[32m>>>SUCCESS<<<\e[0m"
       else
       echo -e "\e[31m>>>Failure<<<\e[0m"
