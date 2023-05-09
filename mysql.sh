3cdd948782d91b6df97c4d2f7a70b0008cd71c6e
Gowthami <kattagowthami1998@gmail.com>
2023-05-10 00:31:15 +0530

    Adde required change

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,12 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
+mysql_root_password=$1
+
+if [ -z "$mysql_root_password" ]; then
+  echo Input MySQL Root Password Missing
+  exit 1
+fi
 
 func_print_head "Disable MySQL 8 Version"
 dnf module disable mysql -y &>>$log_file
