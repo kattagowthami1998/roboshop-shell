bdc6d77071af579372717f792df21d19ad34e38b
Gowthami <kattagowthami1998@gmail.com>
2023-05-10 00:23:13 +0530

    Added required change

--- mysql.sh
+++ mysql.sh
@@ -10,10 +10,10 @@ func_print_head "copying mysql.repo"
 cp ${script_path}/mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 func_stat_check $?
 
-func_print_head "update listen address"
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf  &>>$log_file
-sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf &>>$log_file
-func_stat_check $?
+#func_print_head "update listen address"
+#sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis.conf  &>>$log_file
+#sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/redis/redis.conf &>>$log_file
+#func_stat_check $?
 
 func_print_head "install mysql.repo"
 yum install mysql-community-server -y &>>$log_file
